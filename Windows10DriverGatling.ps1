c317fd0fc0d6f0a635a08694786b53129807a07a
Miles Farber <24918282+MilesFarber@users.noreply.github.com>
2024-05-21 18:30:29 +0200

    Create Windows10DriverGatling.ps1

---
+++ Windows10DriverGatling.ps1
@@ -0,0 +1,42 @@
+Read-Host -Prompt "Press Enter to start the script"
+try {
+	Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+	$CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+	if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+	{
+		$StartTime = Get-Date
+	}
+	else
+	{
+		Write-Output "Running PowerShell Elevated."
+		$ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+		$ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+		$ElevatedProcess.Verb = "runas"
+		[System.Diagnostics.Process]::Start($ElevatedProcess)
+		Exit
+	}
+	
+	$directory = $PSScriptRoot # Get the directory of the script file
+	Write-Output "Script is running in $directory ."
+	$infiles = Get-ChildItem -Path $directory -Filter *.inf -Recurse -File
+	foreach ($infile in $infiles) {
+		Write-Output "Attempting to install $infile . Pass 1..."
+		pnputil.exe /i /a $infile.FullName
+	}
+	foreach ($infile in $infiles) {
+		Write-Output "Attempting to install $infile . Pass 2..."
+		pnputil.exe /i /a $infile.FullName
+	}
+	foreach ($infile in $infiles) {
+		Write-Output "Attempting to install $infile . Pass 3..."
+		pnputil.exe /i /a $infile.FullName
+	}
+	
+	$EndTime = Get-Date
+	$TotalTime = $EndTime - $StartTime
+	Write-Output "All tasks completed! The script wasted $($TotalTime.ToString()) of your life. Feel free to close this window, or wait 12 hours to automatically close it."
+}
+catch {
+	Write-Host $_.Exception.Message
+}
+timeout /t 43210 /nobreak
